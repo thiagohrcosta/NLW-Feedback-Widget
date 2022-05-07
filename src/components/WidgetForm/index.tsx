e61e4bef53d6163c026f55df3aef78fdd8f98d89
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-05-07 15:10:13 -0300

    Adding feedback type

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,58 @@
+import bugImageUrl from "../../assets/bug.svg";
+import IdeaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto'
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: IdeaImageUrl,
+      alt: 'Imagem de uma lâmpada'
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento'
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+        ) : (
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        2022 - Feito por <a className="underline underline-offset-2" href="https://github.com/thiagohrcosta" target="_blank">Thiago H. R. Costa</a>
+      </footer>
+    </div>
+  )
+}
